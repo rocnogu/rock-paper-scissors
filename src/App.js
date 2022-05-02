4386c3b10830316d43876858cf00bd5d04ba9dc4
rocnogu <rocnogu@abv.bg>
2022-05-02 20:10:05 +0200

    commit one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-lone-blocks */
+import "./App.css";
+import { useEffect, useState } from "react";
+////////////////
 
-function App() {
+const App = () => {
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+  const choices = ["rock", "paper", "scissors"];
+
+  const handleClick = (value) => {
+    setUserChoice(value);
+    generateComputerChoice();
+  };
+
+  const generateComputerChoice = () => {
+    const randomNumber = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomNumber);
+  };
+  ////////////////
+
+  useEffect(() => {
+    switch (userChoice + computerChoice) {
+      case "scissorspaper":
+      case "rockscissors":
+      case "paperrock":
+        setResult("WINNER!");
+        break;
+
+      case "paperscissors":
+      case "scissorsrock":
+      case "rockpaper":
+        setResult("LOSER!");
+        break;
+
+      case "rockrock":
+      case "paperpaper":
+      case "scissorsscissors":
+        setResult("DRAW!");
+        break;
+
+      default:
+    }
+  }, [computerChoice, userChoice]);
+  ////////////////
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>YOU: {userChoice}</h1>
+      <h1>computer: {computerChoice}</h1>
+      {choices.map((choice, index) => (
+        <button key={index} onClick={() => handleClick(choice)}>
+          {choice}
+        </button>
+      ))}
+      <h1>{result}</h1>
     </div>
   );
-}
+  ////////////////
+};
+////////////////
 
 export default App;
